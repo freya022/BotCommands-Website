83a97932a8b4dd517319f83be00fc3814222591a
freya02 <41875020+freya022@users.noreply.github.com>
2024-11-25 22:03:28 +0100

    First iteration of an automatic doc linker

---
+++ docs/doc_links.py
@@ -0,0 +1,57 @@
+import logging
+import re
+import requests
+from mkdocs import plugins
+
+
+class IdentifierNotFoundException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+log = logging.getLogger(f"mkdocs.plugins.{__name__}")
+
+errors: list = []
+session = requests.Session()
+
+
+def get_link_representation(identifier: str) -> dict | None:
+    r = session.get('http://localhost:16069/link', params={'identifier': identifier}, headers={'Accept': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        log.debug(f"Received status code {r.status_code} for identifier {identifier}")
+        return None
+    return r.json()
+
+
+def fully_replace_identifier(match: re.Match[str]) -> str | None:
+    identifier = match.group(1)
+    link_representation = get_link_representation(identifier)
+    if link_representation is None:
+        errors.append(identifier)
+        return None
+
+    return f'[`{link_representation["label"]}`]({link_representation["url"]})'
+
+
+def on_pre_build(**kwargs):
+    global errors
+    errors = []
+
+@plugins.event_priority(100)
+def on_page_markdown(markdown: str, **kwargs) -> str:
+    return re.sub(r'\[\[([\w#(), .]+)]]', fully_replace_identifier, markdown)
+
+
+def on_post_build(**kwargs):
+    if len(errors) > 0:
+        raise IdentifierNotFoundException(f"Some identifiers were not found:\n{', '.join(errors)}")
+
+
+def on_shutdown():
+    session.close()
+
+
+if __name__ == '__main__':
+    on_pre_build()
+    print(on_page_markdown(r'[[BService]]'))
+    on_post_build()
+    on_shutdown()
---
+++ src/main/kotlin/io/github/freya022/link/server/LinkServer.kt
@@ -0,0 +1,134 @@
+package io.github.freya022.link.server
+
+import ch.qos.logback.classic.ClassicConstants
+import io.github.classgraph.ClassGraph
+import io.github.freya022.botcommands.api.core.utils.simpleNestedName
+import io.github.freya022.wiki.config.Environment
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.http.*
+import io.ktor.resources.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.resources.*
+import io.ktor.server.resources.Resources
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import kotlin.io.path.absolutePathString
+import kotlin.metadata.ClassKind
+import kotlin.metadata.KmClass
+import kotlin.metadata.jvm.KotlinClassMetadata
+import kotlin.metadata.jvm.moduleName
+import kotlin.metadata.kind
+
+private val logger by lazy { KotlinLogging.logger { } }
+
+private val classes = ClassGraph()
+    .enableClassInfo()
+    .enableAnnotationInfo()
+    .acceptPackages("io.github.freya022.botcommands.api")
+    .scan()
+    .allClasses
+
+@Resource("/link")
+class Link(val identifier: String)
+
+@Serializable
+data class LinkRepresentation(
+    val label: String,
+    val url: String,
+//    val tooltip: String,
+)
+
+fun main() {
+    System.setProperty(ClassicConstants.CONFIG_FILE_PROPERTY, Environment.logbackConfigPath.absolutePathString())
+    logger.info { "Loading logback configuration at ${Environment.logbackConfigPath.absolutePathString()}" }
+
+    embeddedServer(Netty, port = 16069) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(Resources)
+        install(CallLogging)
+
+        routing {
+            get<Link> {
+                val identifier = it.identifier
+                val link = getIdentifierLinkRepresentation(identifier)
+                if (link != null) {
+                    call.respond(link)
+                } else {
+                    call.respondText("Link not found", status = HttpStatusCode.NotFound)
+                }
+            }
+        }
+    }.start(wait = true)
+}
+
+private fun getIdentifierLinkRepresentation(identifier: String): LinkRepresentation? {
+    fun getClassMetadata(className: String): KotlinClassMetadata? {
+        val classInfo = classes.firstOrNull { classInfo -> classInfo.simpleNestedName.replace('$', '.') == className }
+            ?: return null
+        val metadataAnnotation = classInfo.annotationInfo.directOnly()[Metadata::class.java.name].loadClassAndInstantiate() as Metadata
+        return KotlinClassMetadata.readStrict(metadataAnnotation)
+    }
+
+    fun notFound(reason: String): LinkRepresentation? {
+        logger.info { "Could not find '$identifier': $reason" }
+        return null
+    }
+
+    fun String.toKDocCase(): String {
+        return replace(Regex("[A-Z]")) { "-${it.value.lowercase()}" }
+    }
+
+    fun KmClass.getBaseLink(): String {
+        val module = moduleName!!.toKDocCase()
+        val firstUppercaseIndex = name.indexOfFirst { ch -> ch.isUpperCase() }
+        val packageName = name.substring(0..<firstUppercaseIndex - 1).replace('/', '.')
+        val classNames = name.substring(firstUppercaseIndex).split(".").joinToString("/") { it.toKDocCase() }
+        return "https://docs.bc.freya02.dev/$module/$packageName/$classNames"
+    }
+
+    if ('#' in identifier) {
+        val (className, memberName) = identifier.split("#")
+        val metadata = getClassMetadata(className)
+            ?: return notFound("'$className' was not found")
+
+        val kmClass = (metadata as? KotlinClassMetadata.Class)?.kmClass
+            ?: return notFound("'$className' is not a class")
+        val func = kmClass.functions.firstOrNull { function -> function.name == memberName }
+        if (func != null) {
+            return LinkRepresentation(identifier, "${kmClass.getBaseLink()}/${func.name.toKDocCase()}.html")
+        }
+
+        val prop = kmClass.properties.firstOrNull { property -> property.name == memberName }
+        if (prop != null)
+            return LinkRepresentation(identifier, "${kmClass.getBaseLink()}/${prop.name.toKDocCase()}.html")
+
+        val enumEntry = kmClass.enumEntries.firstOrNull { enumEntry -> enumEntry == memberName }
+        if (enumEntry != null)
+            return LinkRepresentation(identifier, "${kmClass.getBaseLink()}/${enumEntry.toKDocCase()}/index.html")
+
+        return notFound("'$memberName' is neither a function, property or enum value in '$className'")
+    } else {
+        val className = identifier
+        val metadata = getClassMetadata(className)
+            ?: return notFound("'$className' was not found")
+
+        val kmClass = (metadata as? KotlinClassMetadata.Class)?.kmClass
+            ?: return notFound("'$className' is not a class")
+
+        return when (kmClass.kind) {
+            ClassKind.ANNOTATION_CLASS -> LinkRepresentation("#!java @$identifier", "${kmClass.getBaseLink()}/index.html")
+            else -> LinkRepresentation(identifier, "${kmClass.getBaseLink()}/index.html")
+        }
+    }
+}
+
+private val KmClass.packageName: String get() = name.takeWhile { !it.isUpperCase() }
+private val KmClass.simpleNestedName: String get() = name.dropWhile { !it.isUpperCase() }
